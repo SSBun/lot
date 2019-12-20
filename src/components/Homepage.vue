2d026c14a3d781f36af395a642f9032a76e82943
蔡士林 <caishilin@babyfs.cn>
2019-12-20 19:20:36 +0800

    initialize

---
+++ src/components/Homepage.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <el-container>
+      <el-header>温湿度监测</el-header>
+      <el-main>
+        <el-row :gutter="20">
+          <el-col :span="12"> <monitor name="阳台温度"/></el-col>
+          <el-col :span="12"> <monitor name="室内温度"/></el-col>
+        </el-row>
+      </el-main>
+    </el-container>
+    <el-row>
+      <el-button type="primary" @click="refreshData"> Refresh Data </el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import Monitor from "./Monitor.vue";
+import { log } from 'util';
+
+export default {
+  components: { Monitor },
+  methods: {
+    refreshData() {
+      this.$axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => {
+        log(response)
+      })
+    }
+  }
+};
+</script>
+
+<style>
+.el-header,
+.el-footer {
+  background-color: #409EFF;
+  border-radius: 8px;
+  color: #fff;
+  font-size: 1.8em;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  /* background-color: #e9eef3; */
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+</style>
